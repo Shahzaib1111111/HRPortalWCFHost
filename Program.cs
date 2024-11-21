b925bd412b477c9ccbb0fe74f205f6a268050e45
Shahzaib1111111 <shahzaibtrafix@gmail.com>
2024-11-21 17:21:54 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+namespace WCFHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri httpUrl = new Uri("http://localhost:8000/WCFSample/ServiceCalc");
+            ServiceHost host = new ServiceHost(typeof(WCFSample.ServiceCalc), httpUrl);
+
+            host.AddServiceEndpoint(typeof(WCFSample.IServiceCalc), new WSHttpBinding(), "");
+
+            ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+            smb.HttpGetEnabled = true;
+            host.Description.Behaviors.Add(smb);
+
+            host.Open();
+            Console.WriteLine("Service is host at " + DateTime.Now.ToString());
+            Console.WriteLine("Host is running... Press  key to stop");
+            Console.ReadLine();
+        }
+    }
+}
